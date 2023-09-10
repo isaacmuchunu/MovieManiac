d98fcf37f8aa38e71d429a17bf14b5ec01c7538d
isaac <isaacmuchunu@gmail.com>
2023-09-10 14:10:31 +0300

    tried implementing darktheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar'
-import { MovieList } from './components/MovieList'
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import { MovieList } from "./components/MovieList";
 
 function App() {
-
   return (
-    <div className='bg-gray-900 '>
-      <Navbar/>
-      <MovieList/>
-   
-   
+    <div className="bg-gray-900">
+      <Navbar />
+      <MovieList />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
